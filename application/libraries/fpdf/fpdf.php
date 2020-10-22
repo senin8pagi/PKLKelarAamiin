d4c608b9e9ba088b0e86c48e26d9fc183cddc379
senin8pagi <73236312+senin8pagi@users.noreply.github.com>
2020-10-22 22:29:07 +0700

    Update fpdf.php

--- application/libraries/fpdf/fpdf.php
+++ application/libraries/fpdf/fpdf.php
@@ -9,8 +9,7 @@
 
 define('FPDF_VERSION','1.82');
 
-class FPDF
-{
+class FPDF {
 protected $page;               // current page number
 protected $n;                  // current object number
 protected $offsets;            // array of object offsets
