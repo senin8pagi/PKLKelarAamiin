3b4eb9348dfe315e4dcb87ba2fa257d8ca42bf8e
senin8pagi <73236312+senin8pagi@users.noreply.github.com>
2020-10-22 19:56:15 +0700

    Update error_404.php

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -57,8 +57,8 @@ p {
 </head>
 <body>
 	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+		<h1><?php return $heading; ?></h1>
+		<?php return $message; ?>
 	</div>
 </body>
 </html>
