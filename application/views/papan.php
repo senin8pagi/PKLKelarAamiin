7c50a4b12b93615e062cda70218ef5205d26b48b
senin8pagi <73236312+senin8pagi@users.noreply.github.com>
2020-10-22 20:48:32 +0700

    Update papan.php

--- application/views/papan.php
+++ application/views/papan.php
@@ -5,7 +5,7 @@
                                                 <select id="id_kelas" name="id_kelas" class="form-control" required>
                                                     <option value="" selected disabled>-- Pilih Tingkatan --</option>
                                                     <?php foreach($kelas->result() as $row){?>
-                                                <option value="<?= $row->id_kelas?>" <?php if(isset($siswa) && $siswa->id_kelas == $row->id_kelas) echo "selected"?>><?= $row->kelas." ".$row->ruang?></option>
+                                                <option value="<?= $row->id_kelas?>" <?php if(isset($siswa) && $siswa->id_kelas == $row->id_kelas) return "selected"?>><?= $row->kelas." ".$row->ruang?></option>
                     <?php } ?>
                                                 </select>
                                         </div>
