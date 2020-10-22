c3c984bf16311e94a0b98df63464f60d53ab189b
senin8pagi <73236312+senin8pagi@users.noreply.github.com>
2020-10-22 16:15:15 +0700

    Update Document.php

--- assets/dompdf/lib/php-svg-lib/src/Svg/Document.php
+++ assets/dompdf/lib/php-svg-lib/src/Svg/Document.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
 
@@ -351,7 +351,7 @@ class Document extends AbstractTag
 
             $tag->handle($attributes);
         } else {
-            echo "Unknown: '$name'\n";
+            return "Unknown: '$name'\n";
         }
     }
 
